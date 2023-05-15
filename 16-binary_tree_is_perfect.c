35cbcc957c728a2c6f708ae914f9a1ae79202209
alejandr088 <alejandrivello@gmail.com>
2023-05-15 13:48:36 -0700

    task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -61,7 +61,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
-		return (1);
+		return (0);
 
 	return (binary_tree_is_full(tree->left) &&
 			binary_tree_is_full(tree->right));
