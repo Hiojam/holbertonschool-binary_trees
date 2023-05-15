5a46bf1a8db67bc6cf73953974f0979ea45e5692
alejandr088 <alejandrivello@gmail.com>
2023-05-15 14:20:08 -0700

    task 16 fixed Betty

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -41,7 +41,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
-	return (binary_tree_is_full(tree->left) && 
+	return (binary_tree_is_full(tree->left) &&
 			binary_tree_is_full(tree->right));
 }
 
@@ -55,15 +55,11 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-
 	if (tree->left == NULL || tree->right == NULL)
 		return (0);
-
 	if (binary_tree_height(tree->left) != binary_tree_height(tree->right))
 		return (0);
-
 	return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
 }
